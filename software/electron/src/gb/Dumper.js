86a8a3c4b14c308a2b57e504e4c54987da6f631b
Rodrigo Alfonso <rodri042@gmail.com>
2019-05-11 05:40:17 -0300

    Using a new parser that allows me to handle file transfers

--- software/electron/src/gb/Dumper.js
+++ software/electron/src/gb/Dumper.js
@@ -6,14 +6,17 @@ import _ from "lodash";
 if (!window.DESKTOP_REQUIRE) throw new Error("Missing node.js access!");
 const EventEmitter = window.DESKTOP_REQUIRE("events");
 const SerialPort = window.DESKTOP_REQUIRE("serialport");
-const Readline = window.DESKTOP_REQUIRE("@serialport/parser-readline");
+const ByteLength = window.DESKTOP_REQUIRE("@serialport/parser-byte-length");
+
+const NEWLINE = "\r\n";
+const WAIT = 500;
 
 export default class Dumper extends EventEmitter {
 	constructor(port, baudRate = 57600) {
 		super();
 
 		const serialPort = new SerialPort(port, { baudRate });
-		const parser = new Readline({ delimiter: "\r\n" });
+		const parser = new ByteLength({ length: 64 });
 		serialPort.pipe(parser);
 
 		serialPort.on("open", () => {
@@ -26,36 +29,59 @@ export default class Dumper extends EventEmitter {
 		});
 
 		parser.on("data", (data) => {
-			if (!_.isString(data)) return;
-
 			console.log("Data:", data);
-			this.emit("line", data.asciiOnly());
+			this.emit("data", data);
 		});
 
 		this.serialPort = serialPort;
+		this.parser = parser;
+
+		window.serialPort = this.serialPort;
+		window.parser = parser;
 	}
 
 	readHeader() {
 		return new Promise((resolve, reject) => {
+			this._cleanBuffer();
 			this.serialPort.write("HEADER");
 
-			const lines = [];
-			this.removeAllListeners("line");
-			this.on("line", (data) => {
-				lines.push(data);
-				if (lines.length === 4) {
-					this.removeAllListeners("line");
-
-					const cartridgeType = lines[1];
-
-					resolve({
-						title: lines[0],
-						cartridgeType: getCartridgeType(cartridgeType),
-						romSize: getRomSize(parseInt(lines[2]), cartridgeType),
-						ramSize: getRamSize(parseInt(lines[3]), cartridgeType)
+			this._readLine().then(({ line: title, i }) => {
+				this._readLine(i).then(({ line: cartridgeType, i }) => {
+					this._readLine(i).then(({ line: romType, i }) => {
+						this._readLine(i).then(({ line: ramType, i }) => {
+							resolve({
+								title,
+								cartridgeType: getCartridgeType(cartridgeType),
+								romSize: getRomSize(parseInt(romType), cartridgeType),
+								ramSize: getRamSize(parseInt(ramType), cartridgeType)
+							});
+						});
 					});
-				}
+				});
 			});
 		});
+	}
+
+	_readLine(startIndex = 0) {
+		return new Promise((resolve) => {
+			const wait = () => {
+				setTimeout(() => {
+					const content = this.parser.buffer.slice(startIndex).toString();
+					const endIndex = content.indexOf(NEWLINE);
+					const line = content.substring(0, endIndex).asciiOnly();
+
+					if (!_.isEmpty(line)) {
+						const i = startIndex + endIndex + NEWLINE.length;
+						resolve({ line, i });
+					} else wait();
+				}, WAIT);
+			};
+			wait();
+		});
+	}
+
+	_cleanBuffer() {
+		this.parser.position = 0;
+		this.parser.buffer.fill(0);
 	}
 }
